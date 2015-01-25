9cd008f64183bc4d32ef95bd5094a83a90593cd3
Shiny Yang <mootothemax@gmail.com>
2015-01-25 05:55:19 -0800

    healthbar

--- Assets/Scripts/EnemyFloatingTargetingUI.cs
+++ Assets/Scripts/EnemyFloatingTargetingUI.cs
@@ -6,13 +6,14 @@ public class EnemyFloatingTargetingUI : MonoBehaviour {
 		
 	public bool _active;
 	[SerializeField] public Animator _animator;
-	
+	[SerializeField] public Slider _healthbar;
 	[SerializeField] private float _max_scale, _min_scale, _min_dist, _max_dist;
 
 	public EnemyFloatingTargetingUI i_initialize(float unit_target_scale) {
 		_active = true;
 		_max_scale *= unit_target_scale;
 		_min_scale *= unit_target_scale;
+		_healthbar.gameObject.SetActive(false);
 		return this;
 	}
 	
@@ -22,6 +23,8 @@ public class EnemyFloatingTargetingUI : MonoBehaviour {
 		dist = Mathf.Clamp(dist,_min_dist,_max_dist);
 		float val = (_max_scale-_min_scale) * (1-(dist-_min_dist)/(_max_dist-_min_dist)) + _min_scale;
 		this.transform.localScale = Util.valv(val);
+		
+		_healthbar.value = 0.5f;
 	}
 	
 	public void fadeout() {
@@ -30,6 +33,10 @@ public class EnemyFloatingTargetingUI : MonoBehaviour {
 	
 	public bool should_remove() {
 		return _animator.GetCurrentAnimatorStateInfo(0).IsName("End");
+	}
+	
+	public void Update() {
+		_healthbar.gameObject.SetActive(_animator.GetCurrentAnimatorStateInfo(0).IsName("reticule_spin_anim_idle"));
 	}
 
 }
